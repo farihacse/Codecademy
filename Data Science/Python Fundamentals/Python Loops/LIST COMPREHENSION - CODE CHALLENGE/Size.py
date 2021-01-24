12179cd5c97b5e3bd4ca73811c7d37e52b3d0fdc
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 20:59:06 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/Size.py
@@ -0,0 +1,9 @@
+"""
+Create a new list named lengths that contains the size of each name in the list of names.
+"""
+
+
+names = ["Elaine", "George", "Jerry", "Cosmo"]
+
+lengths = [len(item) for item in names]
+
