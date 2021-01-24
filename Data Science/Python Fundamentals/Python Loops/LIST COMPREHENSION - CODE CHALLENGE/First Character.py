8aa0ed1c5e2b2a28fea3b31017f9607f8cd08033
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 20:57:15 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/First Character.py
@@ -0,0 +1,10 @@
+"""
+Create a new list named first_character that contains the first character from every name in the list names.
+"""
+
+names = ["Elaine", "George", "Jerry", "Cosmo"]
+
+first_character = [item[0] for item in names]
+
+
+
