4f6b0a01dadcd35d36229a03b16d833ba341871d
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 21:00:54 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/Opposite.py
@@ -0,0 +1,9 @@
+"""
+Create a new list named opposite that contains the opposite boolean for each element in the list booleans.
+"""
+
+booleans = [True, False, True]
+
+opposite = [not item for item in booleans]
+
+
