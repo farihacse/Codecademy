1acf1af1098b709df308da27bb110e0abbeae11c
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 20:55:29 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/Add Hello.py
@@ -0,0 +1,10 @@
+"""
+Create a new list named greetings that adds "Hello, " in front of each name in the list names.
+"""
+
+names = ["Elaine", "George", "Jerry", "Cosmo"]
+
+greetings = ["Hello, " + item for item in names]
+
+
+
