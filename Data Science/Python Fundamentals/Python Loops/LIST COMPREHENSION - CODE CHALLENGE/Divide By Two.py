16e2a488c54cb2c07f63741e097784acae7a594d
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 19:52:51 +0600

    intiial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/Divide By Two.py
@@ -0,0 +1,5 @@
+# Create a new list named divide_by_two that contains half of every element in the list nums. Make sure to divide by 2, not 2.0!
+
+nums = [4, 8, 15, 16, 23, 42]
+
+divide_by_two = [item / 2 for item in nums]
