d152a0167bca541bfd8f77b5312e4ad086b214e1
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-24 21:05:50 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Loops/LIST COMPREHENSION - CODE CHALLENGE/Greater Than Two.py
@@ -0,0 +1,8 @@
+"""
+Create a new list called greater_than_two, in which an entry at position i is True if the entry in nums at position i is greater than 2.
+"""
+
+
+nums = [5, -10, 40, 20, 0]
+
+greater_than_two = [ item > 2 for item in nums]
