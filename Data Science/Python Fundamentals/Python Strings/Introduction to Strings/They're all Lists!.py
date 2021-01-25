6f5c6ab8d1ec7a9282157a9fe2f4d42fc0c8d03d
Fariha Noor <77675309+farihanoor@users.noreply.github.com>
2021-01-25 21:26:55 +0600

    initial commit

---
+++ Data Science/Python Fundamentals/Python Strings/Introduction to Strings/They're all Lists!.py
@@ -0,0 +1,11 @@
+# One of the most common things that are represented by strings are names. Save your name as a string to the variable my_name.
+
+my_name = "Keanu Reeves"
+
+
+# Select the first letter of the variable my_name and save it to first_initial.
+
+first_initial = my_name[0]
+
+
+
